3e42c51d0660aff7ad8bcfa41df0349856f2d51c
Priya Choudhary <priyachoudharyakt@gmail.com>
2022-04-28 18:43:22 +0530

    Updates

---
+++ precache-manifest.f6827c5a04b627844327efa04fc418c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd22af48cd522602e2f8",
+    "url": "/NewsMonkey/static/css/main.9cde98e1.chunk.css"
+  },
+  {
+    "revision": "bd22af48cd522602e2f8",
+    "url": "/NewsMonkey/static/js/main.bd22af48.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "/NewsMonkey/static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "9033ed03fd1b8054eda9",
+    "url": "/NewsMonkey/static/js/2.9033ed03.chunk.js"
+  },
+  {
+    "revision": "fc6ad720d22cb8602a1f",
+    "url": "/NewsMonkey/static/js/runtime~main.fc6ad720.js"
+  },
+  {
+    "revision": "1d7c3a29ef9ed44f1b86f165f75de1bb",
+    "url": "/NewsMonkey/index.html"
+  }
+];
